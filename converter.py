61210ebdab1217d7bcc1ae244b577503d089932f
Tejaswi Seelam <141487874+Tejaswiseelam@users.noreply.github.com>
2025-06-23 16:03:32 +0530

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,10 +1,18 @@
 import argparse
 def main():
-    parse=argparse.ArgumentParser()
-    parse.add_argument("celsius",type=float,help="Temperature in celsius")
-    arg=parse.parse_args()
-    fahrenhiet=convert(arg.celsius)
-    print(f"The Temperature is converted from {fahrenhiet} to {arg.celsius}")
-def convert(c):
+    parser=argparse.ArgumentParser()
+    parse=parser.add_mutually_exclusive_group(required=True)
+    parse.add_argument("--c",type=float,help="Temperature in celsius")
+    parse.add_argument("--f",type=float,help="Temperature in fahrenheit")
+    arg=parser.parse_args()
+    if arg.c is not None:
+        fahrenheit=convert1(arg.c)
+        print(f"{arg.c} in celsius converted into {fahrenheit} fahrenheit")
+    else:
+        celsius=convert2(arg.f)
+        print(f"{arg.f} in fahrenheit converted into {celsius} celsius")
+def convert1(c):
     return (c * 9/5)+32
+def convert2(f):
+    return (f-32)*5/9
 main()
